15383d88adcfa66f23391c9952a57e53d35a0ad8
Lionis Emmanouil Georgios(Akis) <akislionis@gmail.com>
2022-06-30 12:16:34 +0300

    Converting the example to a possible answer

---
+++ earthObservation_test.py
@@ -0,0 +1,37 @@
+from owlready2 import *
+
+onto = get_ontology("file:////media/akis/Ubuntu-Windows/Uni/Chatbot-for-Ontology-Generation/ontologies/earhOvservation_test.owl").load()
+
+with onto:
+    # Q:What questions do you want your ontology to be able to answer
+    # A:Enumerate all remote sensors from satellites ->
+    #  remote sensors, satellites,from
+    class Satellites(Thing):
+        pass
+    class RemoteSensors(Thing):
+        pass
+    class from_(RemoteSensors>>Satellites):
+        pass
+    # Q:What is a remote sensor
+    # A:Remote sensor is x
+    # --------
+    RemoteSensors.comment = ["X"]
+
+    # Q:are theres different types of remote sensoers
+    # A: yes
+    # Q:Enumerate all different types of remote sensors
+    # A: altimeter,optical sensor
+
+    class Altimeter(RemoteSensors):
+        pass
+
+    class OpticalSensor(RemoteSensors):
+        pass
+
+    # Q:Radiometer seems a relevant class, is it a remote sensor?
+    # A: yes
+
+    class Radiometer(RemoteSensors):
+        pass
+
+onto.save()
